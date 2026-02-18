b826a031209586be79f218f7187522c610ad4213
Erdoğan Arıkan <merdo9164@gmail.com>
2026-02-19 01:12:04 +0300

    new project
    Aws

---
+++ devops_004_pipeline/src/main/java/com/erdo/devops_004_pipeline/Devops004PipelineApplication.java
@@ -0,0 +1,13 @@
+package com.erdo.devops_004_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops004PipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops004PipelineApplication.class, args);
+    }
+
+}
---
+++ devops_004_pipeline/src/test/java/com/erdo/devops_004_pipeline/Devops004PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdo.devops_004_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops004PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
