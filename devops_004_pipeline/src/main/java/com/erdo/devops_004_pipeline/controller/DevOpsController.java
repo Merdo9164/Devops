2d6e7d2db38caaf0bda539fb89d4bb42fcec7767
Erdoğan Arıkan <merdo9164@gmail.com>
2026-02-19 15:17:54 +0300

    Aws
    AWS Cloud

---
+++ devops_004_pipeline/src/main/java/com/erdo/devops_004_pipeline/controller/DevOpsController.java
@@ -0,0 +1,25 @@
+package com.erdo.devops_004_pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+//http://localhost:8080
+//http://127.0.0.1:8080
+@RestController
+@RequestMapping
+public class DevOpsController {
+
+    @GetMapping
+    public String devopsHello(){
+
+        return "Hello DevOps Pipeline! :"+ LocalDateTime.now();
+    }
+
+    @GetMapping ("info")
+    public String info(){
+        return "Info : " + LocalDateTime.now();
+    }
+}
